52cf4753f8b5acef4bee124247e99155630ac06f
NataliaGMbtm <natalia.gajda@gmail.com>
2023-09-26 22:00:58 +0200

    dodanie głównych folderów

---
+++ components/component1.js
@@ -0,0 +1,14 @@
+// wzór 1
+
+import { View, Text } from "react-native";
+import React from "react";
+
+const component1 = () => {
+  return (
+    <View>
+      <Text>component1</Text>
+    </View>
+  );
+};
+
+export default component1;
