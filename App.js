c8b19101d20b4ff69a7354a522671a5ea9fe06b2
NataliaGMbtm <natalia.gajda@gmail.com>
2023-09-27 21:43:58 +0200

    + hook z wersji web + tworzenie nawigacji

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Strona główna</Text>
+    </View>
+  );
+}
+
+function PeopleScreen() {
+  return (
+    <View>
+      <Text>Znani aktorzy</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="People" component={PeopleScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ hooks/useMyHook.js
@@ -0,0 +1,35 @@
+//https://api.themoviedb.org/3/person/changes?api_key=5219cb6186dc8cdfa863dcdc035f17f8
+
+import { useEffect, useState } from "react";
+
+const useMyHook = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        url + `?api_key=${process.env.REACT_APP_API_KEY}`
+      );
+      if (!response.ok) {
+        setError("Nie udało się pobrać danych");
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError("Nie udało się pobrać danych");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error };
+};
+
+export default useMyHook;
